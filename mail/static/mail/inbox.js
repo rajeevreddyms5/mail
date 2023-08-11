473af38f7abd2e6c9883ba81e9138500de97dc08
MMEGS <rajeevreddyms5@gmail.com>
2023-08-11 18:10:05 +0530

    half working clickable row

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -108,15 +108,18 @@ function load_mailbox(mailbox) {
         else {
           listItem.style.background = 'white';
         }
+
+        //make row clickable
+        listItem.setAttribute('onclick', "/emails/(emails[i].id)");
         
         //  add data to table
         if (mailbox === 'sent') {
-          listItem.innerHTML = `<th class="text-truncate text-bold" style="max-width:25px;">${emails[i].recipients}</th><td class="text-truncate" style="max-width:80px;">${emails[i].subject}</td><td class="text-muted text-truncate text-right" style="max-width:28px;">${emails[i].timestamp}</td>`;
+          listItem.innerHTML = `<th class="text-truncate text-bold" style="max-width:25px;"><a href="/emails/${emails[i].id}">${emails[i].recipients}</a></th><td class="text-truncate" style="max-width:80px;"><a href="/emails/${emails[i].id}">${emails[i].subject}</a></td><td class="text-muted text-truncate text-right" style="max-width:28px;"><a href="/emails/${emails[i].id}">${emails[i].timestamp}</a></td>`;
           mylist.append(listItem); // add list item to list element
           listItem = document.createElement('tr'); // reset the list item
         }
         else {
-          listItem.innerHTML = `<th class="text-truncate text-bold" style="max-width:25px;">${emails[i].sender}</th><td class="text-truncate" style="max-width:80px;">${emails[i].subject}</td><td class="text-muted text-truncate text-right" style="max-width:28px;">${emails[i].timestamp}</td>`;
+          listItem.innerHTML = `<th class="text-truncate text-bold" style="max-width:25px;"><a href="/emails/${emails[i].id}">${emails[i].sender}</a></th><td class="text-truncate" style="max-width:80px;"><a href="/emails/${emails[i].id}">${emails[i].subject}</a></td><td class="text-muted text-truncate text-right" style="max-width:28px;"><a href="/emails/${emails[i].id}">${emails[i].timestamp}</a></td>`;
           mylist.append(listItem); // add list item to list element
           listItem = document.createElement('tr'); // reset the list item
         }
