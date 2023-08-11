185ac7d1719a433d64aeb5f14a868c399ab36ed4
MMEGS <rajeevreddyms5@gmail.com>
2023-08-11 16:04:07 +0530

    send mail and mailbox is working except read and unread color

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -103,7 +103,7 @@ function load_mailbox(mailbox) {
         listItem.style.borderTop = '2px solid black';
         
         //  add data to table
-        listItem.innerHTML = `<td>To: ${emails[i].recipients}</td><td>Subject: ${emails[i].subject}</td><td>Date: ${emails[i].timestamp}</td>`;
+        listItem.innerHTML = `<th class="text-truncate text-bold" style="max-width:20px;">${emails[i].recipients}</th><td class="text-truncate" style="max-width:90px;">${emails[i].subject}</td><td class="text-muted text-truncate text-right" style="max-width:28px;">${emails[i].timestamp}</td>`;
         mylist.append(listItem); // add list item to list element
         listItem = document.createElement('tr'); // reset the list item
       }
